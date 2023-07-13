69f3db3974e781fc7f26433e05e02ce25566a57b
rhquddus <rhquddus@naver.com>
2023-07-13 11:57:03 +0900

    first commit

---
+++ src/main/java/postproject/PostprojectApplication.java
@@ -0,0 +1,13 @@
+package postproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/postproject/PostprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package postproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
